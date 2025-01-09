4ff02cf33c973fa6e4315ac1c442705d22525543
bruno <1201ba@gmail.com>
2025-01-09 15:04:38 +0100

    Removed SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -43,9 +43,7 @@ class MyApp extends StatelessWidget {
         ],
         theme: AppTheme.lightTheme,
         title: 'Name Age Predictor',
-        home: SafeArea(
-          child: Homepage(),
-        ),
+        home: Homepage(),
       );
     });
   }
